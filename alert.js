ce513f0ff7794d26a591cc4503bbd709ee0ac413
163002029.junayed <junayednoor@gmail.com>
2023-01-03 22:25:06 +0600

    Handle Alert

---
+++ alert.js
@@ -0,0 +1,31 @@
+const { chromium } = require('playwright');
+(async() => { // anonymous arrow function
+  // function code
+
+  // launching browser
+  const browser = await chromium.launch({headless:false, slowMo:600});
+  // creating a page inside browser
+  const page = await browser.newPage();
+   // navigating to site
+  await page.goto('https://demoqa.com/alerts');
+  // code to handle the alerts
+
+  // use once to only use the listener once
+  // accept dialog
+  page.once('dialog', async dialog =>{
+      console.log(dialog.message());
+      await dialog.accept();
+  });
+  await page.click('#confirmButton');
+  
+  // enter text and accept dialog
+  page.once('dialog', async dialog =>{
+      console.log(dialog.message());
+      await dialog.accept('my text is this');
+  });
+  await page.click('#promtButton');
+
+  // closing browser
+  await browser.close();
+
+})(); // function calls itself
